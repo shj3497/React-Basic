6ba66c27b9a89a677db7d4ad1668af660365b752
shj3497 <shj3497@gmail.com>
2021-04-30 22:44:12 +0900

    Habits에서 이벤트 처리함수 Update

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
 
+// Habit 컴포넌트의 부모 컴포넌트는 Habits이다.
+// 부모컴포넌트의 데이터 호출시에는 props를 활용하고, 자신의 데이터를 호출할 때는 state를 사용한다.
+
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
 
   handleIncrement = () => {
-    this.setState({
-      count: this.state.count + 1,
-
-    })
+    this.props.onIncrement(this.props.habit)
   }
 
   handleDecrement = () => {
-    const count = this.state.count -1;
-    this.setState({
-      count: count < 0 ? 0 : count
-    })
+    this.props.onDecrement(this.props.habit)
   }
 
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit)
+  }
 
   render() {
     // console.log(this.props.habit);
     // const habitName = this.props.habit.name;
     const {name, count} = this.props.habit;
+
     return (
       <li className="habit">
         <span className="habit-name">{name}</span>
@@ -37,7 +35,7 @@ class Habit extends Component {
           <i className="fas fa-minus-square"></i>
         </button>
 
-        <button className="habit-button habit-delete">
+        <button className="habit-button habit-delete" onClick={this.handleDelete}>
           <i className="fas fa-trash"></i>
         </button>
       </li>
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import Habit from './habit'
 
+// Habits라는 컴포넌트 생성 후 , 기존에 있던 Habit이라는 컴포넌트를 자식 컴포넌트로 만들었다.
+// 자식 컴포넌트에서 부모컴포넌트에 접근 할 시에는 props 로 호출한다.
+// 
+
 export default class Habits extends Component {
   state = {
     habits: [
@@ -8,13 +12,55 @@ export default class Habits extends Component {
       {id:2, name: 'Running', count: 0},
       {id:3, name: 'Coding', count: 0}
     ],
+  };
+
+  handleIncrement = (habit) => {
+    console.log(`handleIncrement : ${habit.name}`);
+    const habits = [...this.state.habits];
+    // console.log(habits); ...의 의미는 그대로 복사해온다라는 뜻
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({
+      habits: habits
+    });
   }
+
+  handleDecrement = (habit) => {
+    console.log(`handleDecrement : ${habit.name}`)
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count--;
+    if(habits[index].count < 0){
+      habits[index].count=0;
+    }
+    this.setState({
+      habits:habits
+    })
+  }
+
+  handleDelete = (habit) => {
+    console.log(`handleDelete : ${habit.name}`)
+    const habits = [...this.state.habits]
+    const index = habits.indexOf(habit);
+    habits.splice(index, 1);
+    this.setState({
+      habits:habits
+    })
+  }
+
+
   render() {
     return (
       <ul>
         {
           this.state.habits.map(habit => (
-            <Habit key={habit.id} habit={habit}  />
+            <Habit 
+              key={habit.id} 
+              habit={habit} 
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete} 
+            />
             )
           )
         }
